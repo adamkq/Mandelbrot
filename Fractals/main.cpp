e6d540476b42846fb815a2bf22a80145fb3bf301
Adam K-Q <adamkq@icloud.com>
2022-04-18 22:52:04 -0700

    Initial Commit

---
+++ Fractals/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Fractals
+//
+//  Created by Adam Kilbourne on 4/18/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
